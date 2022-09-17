2a5fa1acb1127e117553a46d1496864f51846330
SamuelJAmos <79687637+SamuelJAmos@users.noreply.github.com>
2022-09-17 14:54:37 -0400

    Render component set up, changes in configs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import { Footer, Loader, Navbar, Services, Transactions, Welcome } from './components';
+import { Footer, Navbar, Services, Transactions, Welcome } from './components';
 
-const App = () => {
-  return (
-    <div className="min-h-screen">
-      <div className='gradient-bg-welcome'>
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Transactions />
-      <Footer />
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  );
-};
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
 export default App;
